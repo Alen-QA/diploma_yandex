6feeee8723f67add79576930bc1f6cbb9f842649
Alen-QA <140253981+Alen-QA@users.noreply.github.com>
2023-08-13 22:11:26 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://491e0e4e-c263-4f0c-b610-4b3e06c5cd21.serverhub.praktikum-services.ru' #тестовый стенд
+CREATE_ORDER_PATH = '/api/v1/orders'  #эндпоинт создания заказа
+GET_ORDER_PATH = '/v1/orders/track?t=' #получение заказа по номеру
+
+
+
